1bd700c70ccf93c3193980819a9b6af14359bf34
Gongascgomes <115499328+Gongascgomes@users.noreply.github.com>
2025-03-04 01:30:14 +0000

    commit estupido

---
+++ Chinatown_AB/Source/Chinatown_AB/Chinatown_AB.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Chinatown_AB.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Chinatown_AB, "Chinatown_AB" );
