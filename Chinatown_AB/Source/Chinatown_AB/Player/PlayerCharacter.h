3174d5b3b5e0f9e7fa165f48e41370f27e5263db
Gongascgomes <115499328+Gongascgomes@users.noreply.github.com>
2025-03-06 09:16:42 +0000

    commit to main with problem

--- Chinatown_AB/Source/Chinatown_AB/Player/PlayerCharacter.h
+++ Chinatown_AB/Source/Chinatown_AB/Player/PlayerCharacter.h
@@ -4,12 +4,20 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 #include "PlayerCharacter.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class CHINATOWN_AB_API APlayerCharacter : public ACharacter
 {
 	GENERATED_BODY()
+
+	class UInputAction* MoveAction;
+	class UInputAction* LookAction;
+	class UInputAction* JumpAction;
 
 public:
 	// Sets default values for this character's properties
@@ -18,6 +26,21 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputMappingContext* PlayerCharacterContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* LookAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* MovementAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* JumpAction;
+
+
+
 
 public:	
 	// Called every frame
